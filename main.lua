ab779660149ea0644c758171865e24e97ce75ce1
donchokella <ali.alparslan@outlook.com>
2021-10-13 16:00:23 +0300

    score table added, players can move now

--- main.lua
+++ main.lua
@@ -34,9 +34,9 @@ paddle_speed = 200      -- speed at which we will move our paddle; multiplied by
 --[[
     Runs when the game first starts up.
 ]]
-s
+
 function love.load()
-    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.graphics.setDefaultFilter('nearest', 'nearest')  -- I don't why ???
 
     smallFont = love.graphics.newFont('font.ttf', 8)    -- a "retro-looking" font object
 
@@ -47,7 +47,7 @@ function love.load()
 
 -- initialize the window with virtual resulation
 
-    push: setupScreen(virtual_width, virtual_height, window_width, window_height, {
+    push:setupScreen(virtual_width, virtual_height, window_width, window_height, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -66,11 +66,21 @@ end
     frame, which LOVE2D supplies us.
 ]]
 function love.update(dt)
+
+    -- player1 movement
     if love.keyboard.isDown('w') then
         player1Y = player1Y + -paddle_speed*dt      -- negative paddle speed since the y axis is upside down for the LOVE2D
     elseif love.keyboard.isDown('s') then
         player1Y = player1Y + paddle_speed*dt
     end
+
+    -- player2 movement
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -paddle_speed*dt      -- negative paddle speed since the y axis is upside down for the LOVE2D
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + paddle_speed*dt
+    end
+
 
 
 end
@@ -85,17 +95,26 @@ end
 
 function love.draw()
 
-    push: apply('start')        -- begin rendering at virtual resolution
+    push:apply('start')        -- begin rendering at virtual resolution
     
     love.graphics.clear(40/255, 45/255, 53/255, 255/255)    -- clear the screen with a color that similar the original pong game
+    
+    -- welcome text on the top of the screen
+    love.graphics.setFont(smallFont)
     love.graphics.printf('HELL-Looo Player!!!', 0, 20, virtual_width, 'center')
+
+    -- score text on the middle left and middle right
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), virtual_width/2 - 50, virtual_height/3)
+    love.graphics.print(tostring(player2Score), virtual_width/2 + 30, virtual_height/3)
+
 
     --[[
         Paddles and the ball are simply rectangles
     ]]
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
-    love.graphics.rectangle('fill', virtual_width - 10, virtual_height - 50, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    love.graphics.rectangle('fill', virtual_width - 10, player2Y, 5, 20)
     love.graphics.rectangle('fill', virtual_width/2 - 2, virtual_height/2 - 2, 4, 4)
 
-    push: apply('end')
+    push:apply('end')
 end
