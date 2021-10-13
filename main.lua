2ad91a0f9c682739c3fb6e82ee2e3c25630f9790
donchokella <ali.alparslan@outlook.com>
2021-10-13 11:18:56 +0300

    paddles, the ball are added; screen color changed

--- main.lua
+++ main.lua
@@ -35,7 +35,7 @@ virtual_height = 243
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    smallFont = love.graphics.newFont('font.ttf', 30)   -- a "retro-looking" font object
+    smallFont = love.graphics.newFont('font.ttf', 8)   -- a "retro-looking" font object
 
     love.graphics.setFont(smallFont)
 
@@ -51,13 +51,24 @@ end
 
 
 function love.keypressed(key)
-    if key == 'escape' then     --keys  can be accessed by string name
+    if key == 'escape' then     -- keys  can be accessed by string name
         love.event.quit()       -- quit function to terminate the game
     end
 end
 
 function love.draw()
-    love.graphics.printf('HELL-Looo Player!!!', 0, window_height/2-6, window_width, 'center')
 
+    push: apply('start')        -- begin rendering at virtual resolution
+    
+    love.graphics.clear(40/255, 45/255, 53/255, 255/255)    -- clear the screen with a color that similar the original pong game
+    love.graphics.printf('HELL-Looo Player!!!', 0, 20, virtual_width, 'center')
 
+    --[[
+        Paddles and the ball are simply rectangles
+    ]]
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', virtual_width - 10, virtual_height - 50, 5, 20)
+    love.graphics.rectangle('fill', virtual_width/2 - 2, virtual_height/2 - 2, 4, 4)
+
+    push: apply('end')
 end
